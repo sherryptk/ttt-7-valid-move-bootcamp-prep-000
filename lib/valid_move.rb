4a8d1cedd0722b2404dd8210a729bf1e262d76d7
Sherry <sherryptk@gmail.com>
2017-10-07 17:55:48 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@
 def valid_move?(board,index)
   if position_taken?(board, index)==false || position_taken?(board, index)==true && 0>= index && index <= 8
       true
-  elsif !0>= index && !index <= 8
+  elsif index > 8
     false
   else position_taken?(board, index)==true && 0>= index && index <= 8
       false
